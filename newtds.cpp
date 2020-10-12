7653c6ed3822b34ded24da829b4fa76d7823ce51
Mark Harmstone <mark@harmstone.com>
2020-10-12 15:34:31 +0100

    newtds: connect to server

--- newtds.cpp
+++ newtds.cpp
@@ -1,9 +1,106 @@
 #include <iostream>
+#include <string>
+#include <fmt/format.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 using namespace std;
 
+static const string db_server = "luthien", db_user = "sa", db_password = "Password1$";
+static const uint16_t db_port = 1433;
+
+class formatted_error : public exception {
+public:
+    template<typename T, typename... Args>
+    formatted_error(const T& s, Args&&... args) {
+        msg = fmt::format(s, forward<Args>(args)...);
+    }
+
+    const char* what() const noexcept {
+        return msg.c_str();
+    }
+
+private:
+    string msg;
+};
+
+class newtds {
+public:
+    newtds(const string& server, uint16_t port, const string_view& user, const string_view& password) {
+        connect(server, port);
+    }
+
+    ~newtds() {
+        if (sock != 0)
+            close(sock);
+    }
+
+private:
+    void connect(const string& server, uint16_t port) {
+        struct addrinfo hints;
+        struct addrinfo* res;
+        struct addrinfo* orig_res;
+        int ret;
+
+        // FIXME - make sure this works with both IPv4 and IPv6
+
+        memset(&hints, 0, sizeof(hints));
+        hints.ai_family = AF_INET6;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+
+        ret = getaddrinfo(server.c_str(), nullptr, &hints, &res);
+
+        if (ret != 0)
+            throw formatted_error("getaddrinfo returned {}\n", ret);
+
+        orig_res = res;
+        sock = 0;
+
+        do {
+            sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+            if (sock < 0)
+                continue;
+
+            if (res->ai_family == AF_INET)
+                ((struct sockaddr_in*)res->ai_addr)->sin_port = htons(port);
+            else if (res->ai_family == AF_INET6)
+                ((struct sockaddr_in6*)res->ai_addr)->sin6_port = htons(port);
+            else {
+                close(sock);
+                sock = 0;
+                continue;
+            }
+
+            if (::connect(sock, res->ai_addr, res->ai_addrlen) != 0) {
+                close(sock);
+                sock = 0;
+                continue;
+            }
+
+            break;
+        } while (res = res->ai_next);
+
+        freeaddrinfo(orig_res);
+
+        if (sock <= 0)
+            throw formatted_error("Could not connect to {}:{}.", server, port);
+    }
+
+    int sock = 0;
+};
+
 int main() {
-    cout << "Hello world" << endl;
+    try {
+        newtds n(db_server, db_port, db_user, db_password);
+    } catch (const exception& e) {
+        cerr << "Exception: " << e.what() << endl;
+        return 1;
+    }
 
     return 0;
 }
