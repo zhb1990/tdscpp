173d7a3949696f69c1832310be35052946ee8f20
Mark Harmstone <mark@harmstone.com>
2020-10-13 17:12:11 +0100

    newtds: send LOGIN7 message

--- newtds.cpp
+++ newtds.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <codecvt>
 #include <fmt/format.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -99,12 +100,68 @@ enum class tds_encryption_type : uint8_t {
     ENCRYPT_REQ
 };
 
+#pragma pack(push,1)
+
+struct tds_login_msg {
+    uint32_t length;
+    uint32_t tds_version;
+    uint32_t packet_size;
+    uint32_t client_version;
+    uint32_t client_pid;
+    uint32_t connexion_id;
+    uint8_t option_flags1;
+    uint8_t option_flags2;
+    uint8_t sql_type_flags;
+    uint8_t option_flags3;
+    int32_t timezone;
+    uint32_t collation;
+    uint16_t client_name_offset;
+    uint16_t client_name_length;
+    uint16_t username_offset;
+    uint16_t username_length;
+    uint16_t password_offset;
+    uint16_t password_length;
+    uint16_t app_name_offset;
+    uint16_t app_name_length;
+    uint16_t server_name_offset;
+    uint16_t server_name_length;
+    uint16_t unused_offset;
+    uint16_t unused_length;
+    uint16_t interface_library_offset;
+    uint16_t interface_library_length;
+    uint16_t locale_offset;
+    uint16_t locale_length;
+    uint16_t database_offset;
+    uint16_t database_length;
+    uint8_t mac_address[6];
+    uint16_t sspi_offset;
+    uint16_t sspi_length;
+    uint16_t attach_db_offset;
+    uint16_t attach_db_length;
+    uint16_t new_password_offset;
+    uint16_t new_password_length;
+    uint16_t sspi_long_offset;
+    uint16_t sspi_long_length;
+};
+
+#pragma pack(pop)
+
+static_assert(sizeof(tds_login_msg) == 94, "tds_login_msg has wrong size");
+
+static u16string utf8_to_utf16(const string_view& sv) {
+    wstring_convert<codecvt_utf8_utf16<char16_t>, char16_t> convert;
+
+    return convert.from_bytes(sv.data(), sv.data() + sv.length());
+}
+
 class newtds {
 public:
     newtds(const string& server, uint16_t port, const string_view& user, const string_view& password) {
         connect(server, port);
 
         send_prelogin_msg();
+
+        send_login_msg(user, password);
     }
 
     ~newtds() {
@@ -187,6 +244,7 @@ private:
 
         // encryption
         // FIXME - actually support encryption
+        // FIXME - handle error message if server insists on encryption
 
         enc = tds_encryption_type::ENCRYPT_NOT_SUP;
 
@@ -194,7 +252,8 @@ private:
 
         // instopt
 
-        opts.emplace_back(newtds_login_opt_type::instopt, instance);
+        // needs trailing zero
+        opts.emplace_back(newtds_login_opt_type::instopt, string_view{instance, sizeof(instance)});
 
         // MARS
 
@@ -226,6 +285,227 @@ private:
         tlo->type = newtds_login_opt_type::terminator;
 
         send_msg(newtds_msg::prelogin, msg);
+
+        {
+            enum newtds_msg type;
+            string payload;
+
+            wait_for_msg(type, payload);
+            // FIXME - timeout
+
+            if (type != newtds_msg::tabular_result)
+                throw formatted_error(FMT_STRING("Received message type {}, expected tabular_result"), (int)type);
+
+            // FIXME - parse payload for anything we care about (in particular, what server says about encryption)
+        }
+    }
+
+    void send_login_msg(const string_view& user, const string_view& password) {
+        enum newtds_msg type;
+        string payload;
+
+        // FIXME - support SSPI
+
+        auto user_u16 = utf8_to_utf16(user);
+        auto password_u16 = utf8_to_utf16(password);
+
+        // FIXME - client PID
+        // FIXME - option flags (1, 2, 3)
+        // FIXME - collation
+        // FIXME - client name
+        // FIXME - app name
+        // FIXME - server name
+        // FIXME - locale name?
+
+        send_login_msg2(0x74000004, 4096, 0xf8f28306, 0x5ab7, 0, 0xe0, 0x03, 0, 0x08, 0x436,
+                        u"beren", user_u16, password_u16, u"test program", u"luthien", u"", u"us_english",
+                        u"", u"", u"");
+
+        wait_for_msg(type, payload);
+        // FIXME - timeout
+        // FIXME - parse
+    }
+
+    void send_login_msg2(uint32_t tds_version, uint32_t packet_size, uint32_t client_version, uint32_t client_pid,
+                         uint32_t connexion_id, uint8_t option_flags1, uint8_t option_flags2, uint8_t sql_type_flags,
+                         uint8_t option_flags3, uint32_t collation, const u16string_view& client_name,
+                         const u16string_view& username, const u16string_view& password, const u16string_view& app_name,
+                         const u16string_view& server_name, const u16string_view& interface_library,
+                         const u16string_view& locale, const u16string_view& database, const u16string_view& attach_db,
+                         const u16string_view& new_password) {
+        uint32_t length, off;
+
+        // FIXME - send features list (UTF-8 support etc.)
+
+        length = sizeof(tds_login_msg);
+        length += client_name.length() * sizeof(char16_t);
+        length += username.length() * sizeof(char16_t);
+        length += password.length() * sizeof(char16_t);
+        length += app_name.length() * sizeof(char16_t);
+        length += server_name.length() * sizeof(char16_t);
+        length += interface_library.length() * sizeof(char16_t);
+        length += locale.length() * sizeof(char16_t);
+        length += database.length() * sizeof(char16_t);
+
+        string payload;
+
+        payload.resize(length);
+
+        auto msg = (tds_login_msg*)payload.data();
+
+        msg->length = length;
+        msg->tds_version = tds_version;
+        msg->packet_size = packet_size;
+        msg->client_version = client_version;
+        msg->client_pid = client_pid;
+        msg->connexion_id = connexion_id;
+        msg->option_flags1 = option_flags1;
+        msg->option_flags2 = option_flags2;
+        msg->sql_type_flags = sql_type_flags;
+        msg->option_flags3 = option_flags3;
+        msg->timezone = 0;
+        msg->collation = collation;
+
+        off = sizeof(tds_login_msg);
+
+        msg->client_name_offset = off;
+
+        if (!client_name.empty()) {
+            msg->client_name_length = client_name.length();
+            memcpy((uint8_t*)msg + msg->client_name_offset, client_name.data(),
+                   client_name.length() * sizeof(char16_t));
+
+            off += client_name.length() * sizeof(char16_t);
+        } else
+            msg->client_name_length = 0;
+
+        msg->username_offset = off;
+
+        if (!username.empty()) {
+            msg->username_length = username.length();
+            memcpy((uint8_t*)msg + msg->username_offset, username.data(),
+                   username.length() * sizeof(char16_t));
+
+            off += username.length() * sizeof(char16_t);
+        } else
+            msg->username_length = 0;
+
+        msg->password_offset = off;
+
+        if (!password.empty()) {
+            msg->password_length = password.length();
+
+            auto pw_dest = (uint8_t*)msg + msg->password_offset;
+            auto pw_src = (uint8_t*)password.data();
+
+            for (unsigned int i = 0; i < password.length() * sizeof(char16_t); i++) {
+                uint8_t c = *pw_src;
+
+                c = ((c & 0xf) << 4) | (c >> 4);
+                c ^= 0xa5;
+
+                *pw_dest = c;
+
+                pw_src++;
+                pw_dest++;
+            }
+
+            off += password.length() * sizeof(char16_t);
+        } else
+            msg->password_length = 0;
+
+        msg->app_name_offset = off;
+
+        if (!app_name.empty()) {
+            msg->app_name_length = app_name.length();
+            memcpy((uint8_t*)msg + msg->app_name_offset, app_name.data(),
+                   app_name.length() * sizeof(char16_t));
+
+            off += app_name.length() * sizeof(char16_t);
+        } else
+            msg->app_name_length = 0;
+
+        msg->server_name_offset = off;
+
+        if (!server_name.empty()) {
+            msg->server_name_length = server_name.length();
+            memcpy((uint8_t*)msg + msg->server_name_offset, server_name.data(),
+                   server_name.length() * sizeof(char16_t));
+
+            off += server_name.length() * sizeof(char16_t);
+        } else
+            msg->server_name_length = 0;
+
+        msg->unused_offset = 0;
+        msg->unused_length = 0;
+
+        msg->interface_library_offset = off;
+
+        if (!interface_library.empty()) {
+            msg->interface_library_length = interface_library.length();
+            memcpy((uint8_t*)msg + msg->interface_library_offset, interface_library.data(),
+                   interface_library.length() * sizeof(char16_t));
+
+            off += interface_library.length() * sizeof(char16_t);
+        } else
+            msg->interface_library_length = 0;
+
+        msg->locale_offset = off;
+
+        if (!locale.empty()) {
+            msg->locale_length = locale.length();
+            memcpy((uint8_t*)msg + msg->locale_offset, locale.data(),
+                   locale.length() * sizeof(char16_t));
+
+            off += locale.length() * sizeof(char16_t);
+        } else
+            msg->locale_length = 0;
+
+        msg->database_offset = off;
+
+        if (!database.empty()) {
+            msg->database_length = database.length();
+            memcpy((uint8_t*)msg + msg->database_offset, database.data(),
+                   database.length() * sizeof(char16_t));
+
+            off += database.length() * sizeof(char16_t);
+        } else
+            msg->database_length = 0;
+
+        // FIXME - set MAC address properly?
+        memset(msg->mac_address, 0, 6);
+
+        // FIXME - SSPI
+
+        msg->sspi_offset = 0;
+        msg->sspi_length = 0;
+
+        msg->attach_db_offset = off;
+
+        if (!attach_db.empty()) {
+            msg->attach_db_length = attach_db.length();
+            memcpy((uint8_t*)msg + msg->attach_db_offset, attach_db.data(),
+                   attach_db.length() * sizeof(char16_t));
+
+            off += attach_db.length() * sizeof(char16_t);
+        } else
+            msg->attach_db_length = 0;
+
+        msg->new_password_offset = off;
+
+        if (!new_password.empty()) {
+            msg->new_password_length = new_password.length();
+            memcpy((uint8_t*)msg + msg->new_password_offset, new_password.data(),
+                   new_password.length() * sizeof(char16_t));
+
+            off += new_password.length() * sizeof(char16_t);
+        } else
+            msg->new_password_length = 0;
+
+        msg->sspi_long_offset = 0;
+        msg->sspi_long_length = 0;
+
+        send_msg(newtds_msg::tds7_login, payload);
     }
 
     void send_msg(enum newtds_msg type, const string_view& msg) {
@@ -252,6 +532,46 @@ private:
 
         if (ret < payload.length())
             throw formatted_error(FMT_STRING("send sent {} bytes, expected {}"), ret, payload.length());
+    }
+
+    void wait_for_msg(enum newtds_msg& type, string& payload) {
+        tds_header h;
+
+        auto ret = recv(sock, &h, sizeof(tds_header), MSG_WAITALL);
+
+        if (ret == -1)
+            throw formatted_error(FMT_STRING("recv failed (error {})"), errno);
+
+        if (ret == 0)
+            throw formatted_error(FMT_STRING("Disconnected."));
+
+        if (ret < sizeof(h))
+            throw formatted_error(FMT_STRING("recv received {} bytes, expected {}"), ret, sizeof(h));
+
+        if (htons(h.length) < sizeof(tds_header)) {
+            throw formatted_error(FMT_STRING("message length was {}, expected at least {}"),
+                                  htons(h.length), sizeof(tds_header));
+        }
+
+        type = h.type;
+
+        if (htons(h.length) > sizeof(tds_header)) {
+            auto len = htons(h.length) - sizeof(tds_header);
+
+            payload.resize(len);
+
+            ret = recv(sock, payload.data(), len, MSG_WAITALL);
+
+            if (ret == -1)
+                throw formatted_error(FMT_STRING("recv failed (error {})"), errno);
+
+            if (ret == 0)
+                throw formatted_error(FMT_STRING("Disconnected."));
+
+            if (ret < len)
+                throw formatted_error(FMT_STRING("recv received {} bytes, expected {}"), ret, len);
+        } else
+            payload.clear();
     }
 
     int sock = 0;
