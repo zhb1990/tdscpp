29e474bb24c915d641c782c0cc75fdbf3a82788d
Mark Harmstone <mark@harmstone.com>
2020-11-23 10:03:04 +0000

    rpc::wait_for_packet: UNIQUEIDENTIFIER has a size byte

--- src/tdscpp.cpp
+++ src/tdscpp.cpp
@@ -4377,7 +4377,6 @@ namespace tds {
                             case sql_type::FLOAT:
                             case sql_type::SMALLMONEY:
                             case sql_type::BIGINT:
-                            case sql_type::UNIQUEIDENTIFIER:
                             case sql_type::DATE:
                                 // nop
                                 break;
@@ -4390,6 +4389,7 @@ namespace tds {
                             case sql_type::DATETIMEOFFSET:
                             case sql_type::BITN:
                             case sql_type::MONEYN:
+                            case sql_type::UNIQUEIDENTIFIER:
                                 if (sv2.length() < sizeof(uint8_t))
                                     throw formatted_error(FMT_STRING("Short COLMETADATA message ({} bytes left, expected at least 1)."), sv2.length());
 
