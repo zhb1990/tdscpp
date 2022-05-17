fb89d413b9a4d41a5b7de97031415ba732e60097
Mark Harmstone <mark@harmstone.com>
2022-05-17 15:50:36 +0100

    add Unicode tests for tds::value

--- src/tests.cpp
+++ src/tests.cpp
@@ -40,19 +40,39 @@ static_assert(value_test(tds::value{optional<string_view>(nullopt)}, tds::sql_ty
 // FIXME - optional<char*>?
 
 static_assert(value_test(tds::value{u8"hello"s}, tds::sql_type::VARCHAR, false, { 0x68, 0x65, 0x6c, 0x6c, 0x6f })); // u8string
+static_assert(value_test(tds::value{u8"h\u00e9llo"s}, tds::sql_type::VARCHAR, false, { 0x68, 0xc3, 0xa9, 0x6c, 0x6c, 0x6f })); // u8string
+static_assert(value_test(tds::value{u8"h\U0001f525llo"s}, tds::sql_type::VARCHAR, false, { 0x68, 0xf0, 0x9f, 0x94, 0xa5, 0x6c, 0x6c, 0x6f })); // u8string
 static_assert(value_test(tds::value{u8"hello"sv}, tds::sql_type::VARCHAR, false, { 0x68, 0x65, 0x6c, 0x6c, 0x6f })); // u8string_view
+static_assert(value_test(tds::value{u8"h\u00e9llo"sv}, tds::sql_type::VARCHAR, false, { 0x68, 0xc3, 0xa9, 0x6c, 0x6c, 0x6f })); // u8string_view
+static_assert(value_test(tds::value{u8"h\U0001f525llo"sv}, tds::sql_type::VARCHAR, false, { 0x68, 0xf0, 0x9f, 0x94, 0xa5, 0x6c, 0x6c, 0x6f })); // u8string_view
 static_assert(value_test(tds::value{u8"hello"}, tds::sql_type::VARCHAR, false, { 0x68, 0x65, 0x6c, 0x6c, 0x6f })); // const char8_t*
+static_assert(value_test(tds::value{u8"h\u00e9llo"}, tds::sql_type::VARCHAR, false, { 0x68, 0xc3, 0xa9, 0x6c, 0x6c, 0x6f })); // const char8_t*
+static_assert(value_test(tds::value{u8"h\U0001f525llo"}, tds::sql_type::VARCHAR, false, { 0x68, 0xf0, 0x9f, 0x94, 0xa5, 0x6c, 0x6c, 0x6f })); // const char8_t*
 static_assert(value_test(tds::value{optional<u8string>(u8"hello")}, tds::sql_type::VARCHAR, false, { 0x68, 0x65, 0x6c, 0x6c, 0x6f })); // optional<u8string>
+static_assert(value_test(tds::value{optional<u8string>(u8"h\u00e9llo")}, tds::sql_type::VARCHAR, false, { 0x68, 0xc3, 0xa9, 0x6c, 0x6c, 0x6f })); // optional<u8string>
+static_assert(value_test(tds::value{optional<u8string>(u8"h\U0001f525llo")}, tds::sql_type::VARCHAR, false, { 0x68, 0xf0, 0x9f, 0x94, 0xa5, 0x6c, 0x6c, 0x6f })); // optional<u8string>
 static_assert(value_test(tds::value{optional<u8string>(nullopt)}, tds::sql_type::VARCHAR, true, { })); // optional<u8string>
 static_assert(value_test(tds::value{optional<u8string_view>(u8"hello")}, tds::sql_type::VARCHAR, false, { 0x68, 0x65, 0x6c, 0x6c, 0x6f })); // optional<u8string_view>
+static_assert(value_test(tds::value{optional<u8string_view>(u8"h\u00e9llo")}, tds::sql_type::VARCHAR, false, { 0x68, 0xc3, 0xa9, 0x6c, 0x6c, 0x6f })); // optional<u8string_view>
+static_assert(value_test(tds::value{optional<u8string_view>(u8"h\U0001f525llo")}, tds::sql_type::VARCHAR, false, { 0x68, 0xf0, 0x9f, 0x94, 0xa5, 0x6c, 0x6c, 0x6f })); // optional<u8string_view>
 static_assert(value_test(tds::value{optional<u8string_view>(nullopt)}, tds::sql_type::VARCHAR, true, { })); // optional<u8string_view>
 // FIXME - optional<char8_t*>?
 
 static_assert(value_test(tds::value{u"hello"s}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // u16string
+static_assert(value_test(tds::value{u"h\u00e9llo"s}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0xe9, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // u16string
+static_assert(value_test(tds::value{u"h\U0001f525llo"s}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0x3d, 0xd8, 0x25, 0xdd, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // u16string
 static_assert(value_test(tds::value{u"hello"sv}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // u16string_view
+static_assert(value_test(tds::value{u"h\u00e9llo"sv}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0xe9, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // u16string_view
+static_assert(value_test(tds::value{u"h\U0001f525llo"sv}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0x3d, 0xd8, 0x25, 0xdd, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // u16string_view
 static_assert(value_test(tds::value{u"hello"}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // char16_t*
+static_assert(value_test(tds::value{u"h\u00e9llo"}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0xe9, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // char16_t*
+static_assert(value_test(tds::value{u"h\U0001f525llo"}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0x3d, 0xd8, 0x25, 0xdd, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // char16_t*
 static_assert(value_test(tds::value{optional<u16string>(u"hello")}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // optional<u16string>
+static_assert(value_test(tds::value{optional<u16string>(u"h\u00e9llo")}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0xe9, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // optional<u16string>
+static_assert(value_test(tds::value{optional<u16string>(u"h\U0001f525llo")}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0x3d, 0xd8, 0x25, 0xdd, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // optional<u16string>
 static_assert(value_test(tds::value{optional<u16string>(nullopt)}, tds::sql_type::NVARCHAR, true, { })); // optional<u16string>
 static_assert(value_test(tds::value{optional<u16string_view>(u"hello")}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // optional<u16string>
+static_assert(value_test(tds::value{optional<u16string_view>(u"h\u00e9llo")}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0xe9, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // optional<u16string>
+static_assert(value_test(tds::value{optional<u16string_view>(u"h\U0001f525llo")}, tds::sql_type::NVARCHAR, false, { 0x68, 0x00, 0x3d, 0xd8, 0x25, 0xdd, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00 })); // optional<u16string>
 static_assert(value_test(tds::value{optional<u16string_view>(nullopt)}, tds::sql_type::NVARCHAR, true, { })); // optional<u16string>
 // FIXME - optional<char16_t*>?
