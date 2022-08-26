23fe381980ed751b8ed2afca7f07d2b9a589e69f
Mark Harmstone <mark@harmstone.com>
2022-08-26 17:28:43 +0100

    rpc::do_rpc: fix UBSAN warning

--- src/tdscpp.cpp
+++ src/tdscpp.cpp
@@ -4147,7 +4147,7 @@ namespace tds {
 
                         ptr += sizeof(tds_VARCHAR_param) - sizeof(tds_param_header);
 
-                        if (!p.is_null) {
+                        if (!p.is_null && h2->length > 0) {
                             memcpy(ptr, p.val.data(), h2->length);
                             ptr += h2->length;
                         }
